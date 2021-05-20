8354d90d1e3766b82cecffe9a76f61580ba3fd35
Grande1988 <legrand.redd@byui.edu>
2021-05-19 19:20:54 -0600

    Added reply sentence to Harrison's sentence.

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("hello, my name is Harrison Bowlby.");
+        System.out.println("Hello Harrison, my name is LeGrand!");
     }
 }
